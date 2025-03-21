ff62be7c9d96372031f0cd8c1c1729819924df7d
”DmitryTikhomirov” <amid4485@yandex.ru>
2025-03-21 13:41:27 +0300

    test Git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.logger.Logger;
 import com.company.menu.dialog.MainStartDialog;
 import com.company.service.impl.UserInputServiceImpl;
+//  Проверка Git
 
 public class Main {
     public static void main(String[] args) {
